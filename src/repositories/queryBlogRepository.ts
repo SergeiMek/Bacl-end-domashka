7f152d2835c882b6617b6830bded167e1f04f44f
serakss <serak_96@mail.ru>
2024-01-09 03:03:55 +0300

    refactor3

--- src/repositories/queryBlogRepository.ts
+++ src/repositories/queryBlogRepository.ts
@@ -27,7 +27,7 @@ export class QueryBlogRepository {
 
         const blogs: Array<blogsType> = await blogsCollection
             .find(filter)
-            .sort({'createdAt':1})
+            .sort({createdAt:1})
             .skip((+pageNumber - 1) * +pageSize)
             .limit(+pageSize)
             .toArray()
