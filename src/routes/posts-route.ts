e33357bf5963874327358f311d65f6acfd806338
serakss <serak_96@mail.ru>
2024-02-15 15:56:43 +0300

    HW6

--- src/routes/posts-route.ts
+++ src/routes/posts-route.ts
@@ -91,10 +91,7 @@ postsRoute.post('/:id/comments', accessTokenGuard, commentValidation(), async (r
     if (!postId) {
        return  res.sendStatus(404)
     }
-    const post =await QueryPostRepository.getPostById(postId)
-    if(!post){
-        return res.sendStatus(404)
-    }
+
     const comment = await CommentsService.sendComment(content, userId, postId)
 
     if (!comment) {
