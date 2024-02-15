ac31aa019a35aae67ae9de38130fddbf7813ec16
serakss <serak_96@mail.ru>
2024-02-15 03:15:55 +0300

    HW6

--- src/validators/comments-validator.ts
+++ src/validators/comments-validator.ts
@@ -3,19 +3,19 @@ import {QueryPostRepository} from "../repositories/queryPostRepository";
 import {inputModelValidation} from "../middlewares/inputModel/input-model-validation";
 
 
-/*const incorrectPostIdValidation = param('id').isString().trim().custom(async (value) => {
+const incorrectPostIdValidation = param('id').isString().trim().custom(async (value) => {
     const post =await QueryPostRepository.getPostById(value)
 debugger
     if (!post) {
         throw new Error("Incorrect postId")
     }
     return true
-}).withMessage("Incorrect postId")*/
+}).withMessage("Incorrect postId")
 
 
 export const contendValidation = body('content').isString().trim().isLength({min: 20, max: 300}).withMessage('Incorrect comment!')
-//export const commentValidation = ()=>[incorrectPostIdValidation,contendValidation,inputModelValidation]
-export const commentValidation = ()=>[contendValidation,inputModelValidation]
+export const commentValidation = ()=>[incorrectPostIdValidation,contendValidation,inputModelValidation]
+//export const commentValidation = ()=>[contendValidation,inputModelValidation]
 export const updateCommentValidation = ()=>[contendValidation,inputModelValidation]
 
 
